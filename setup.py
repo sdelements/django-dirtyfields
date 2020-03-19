33b96535c04e027d2a58bebca7550fa9f3a1e4c3
Andrew Jung <andrewdhjung@gmail.com>
2020-03-19 12:11:24 -0400

    Change tag convention instead of version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def listify(filename):
 
 setup(
     name="django-dirtyfields",
-    version="1.4.0dj2",
+    version="1.3.1.dj2",
     url='http://github.com/romgar/django-dirtyfields',
     license='BSD',
     description=("Tracking dirty fields on a Django model instance "
